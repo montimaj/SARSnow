98ad606faa244136ee70a8122adac9978a55a850
montimaj <monti.majumdar@gmail.com>
2018-12-12 20:09:19 +0530

    Modularised code

--- freshsnow.py
+++ freshsnow.py
@@ -70,24 +70,23 @@ def get_gaussian_kernel(ksize, nsig=3):
 #     fs_neighbor = get_ensemble_window(cpd_data, pos, size)
 #     return len(fs_neighbor[fs_neighbor > 0]) > 0
 
-def get_ensemble_avg(image_arr, wsize=10, nsig=3):
+def get_ensemble_avg(image_arr, wsize=(10, 10), nsig=3):
     print('PERFORMING ENSEMBLE AVERAGING...')
     image_arr = np.transpose(image_arr)
     max_x, max_y = image_arr.shape
     max_x += 1
     max_y += 1
     emat = np.full_like(image_arr, NO_DATA_VALUE, dtype=np.float32)
-    kshape = (wsize, wsize)
     for index, value in np.ndenumerate(image_arr):
         if not np.isnan(value):
-            startx = index[0] - kshape[0]
-            starty = index[1] - kshape[1]
+            startx = index[0] - wsize[0]
+            starty = index[1] - wsize[1]
             if startx < 0:
                 startx = 0
             if starty < 0:
                 starty = 0
-            endx = index[0] + kshape[0] + 1
-            endy = index[1] + kshape[1] + 1
+            endx = index[0] + wsize[0] + 1
+            endy = index[1] + wsize[1] + 1
             if endx > max_x + 1:
                 endx = max_x + 1
             if endy > max_y + 1:
@@ -100,25 +99,34 @@ def get_ensemble_avg(image_arr, wsize=10, nsig=3):
     return np.transpose(emat)
 
 
-def cpd2freshsnow(cpd_file_tdx, cpd_file_tsx, layover_file, outfile, neighborhood_size=11, axial_ratio=2, shape='o'):
+def do_averaging(cpd_file_tdx, cpd_file_tsx, outfile_cpd, outfile_lia):
     print('LOADING FILES TO MEMORY...')
 
     cpd_file_tdx = gdal.Open(cpd_file_tdx)
     cpd_file_tsx = gdal.Open(cpd_file_tsx)
-    layover_file = gdal.Open(layover_file)
     cpd_tdx = cpd_file_tdx.GetRasterBand(1).ReadAsArray()
     cpd_tsx = cpd_file_tsx.GetRasterBand(1).ReadAsArray()
     lia_tdx = cpd_file_tdx.GetRasterBand(3).ReadAsArray()
     lia_tsx = cpd_file_tsx.GetRasterBand(3).ReadAsArray()
+
+    print('ALL FILES LOADED... AVERAGING...')
+    cpd_data = get_ensemble_avg((cpd_tdx + cpd_tsx) / 2.)
+    lia_data = (lia_tdx + lia_tsx) / 2.
+    print('Writing avg data...')
+    write_tif(cpd_data, cpd_file_tdx, outfile_cpd)
+    write_tif(lia_data, cpd_file_tdx, outfile_lia)
+
+
+def cpd2freshsnow(avg_cpd_file, avg_lia_file, layover_file, outfile, axial_ratio=2, shape='o'):
+    print('LOADING FILES ...')
+    avg_cpd_file = gdal.Open(avg_cpd_file)
+    avg_lia_file = gdal.Open(avg_lia_file)
+    layover_file = gdal.Open(layover_file)
+    cpd_data = avg_cpd_file.GetRasterBand(1).ReadAsArray()
+    lia_data = avg_lia_file.GetRasterBand(1).ReadAsArray()
     layover_arr = layover_file.GetRasterBand(1).ReadAsArray()
 
     print('ALL FILES LOADED... CALCULATING PARAMETERS...')
-    cpd_data = get_ensemble_avg((cpd_tdx + cpd_tsx) / 2., 10)
-    lia_data = (lia_tdx + lia_tsx) / 2.
-    print('Writing avg data...')
-    write_tif(cpd_data, cpd_file_tdx, 'Fresh_Snow/cpd_avg_gkernel', -32768)
-    write_tif(lia_data, cpd_file_tdx, 'lia_avg', -32768)
-
     fvol = SNOW_DENSITY/ICE_DENSITY
     depolarisation_factors = get_depolarisation_factor(axial_ratio, shape)
     print('DEPOLARISATION FACTORS = ', depolarisation_factors)
@@ -128,25 +136,24 @@ def cpd2freshsnow(cpd_file_tdx, cpd_file_tsx, layover_file, outfile, neighborhoo
 
     print('PIXELWISE COMPUTATION STARTED...')
     print('Mean incidence angle=', MEAN_INC_ANGLE)
-    sin_inc_sq = np.sin(MEAN_INC_ANGLE) ** 2
-    effH = effx
-    effV = effy * np.cos(MEAN_INC_ANGLE) ** 2 + effz * sin_inc_sq
-    xeta_diff = np.sqrt(effV - sin_inc_sq) - np.sqrt(effH - sin_inc_sq)
-    print('Effective permittivities=', str(effH), str(effV))
-    print('Del Xeta=', str(xeta_diff))
     cols, rows = cpd_data.shape
     fresh_sd = np.zeros(cpd_data.shape).astype(np.float32)
     for i in range(rows):
         for j in range(cols):
             cpd = cpd_data[j, i]
             if layover_arr[j, i] == 0. and cpd > 0:
+                sin_inc_sq = np.sin(lia_data[j, i]) ** 2
+                effH = effx
+                effV = effy * np.cos(lia_data[j, i]) ** 2 + effz * sin_inc_sq
+                xeta_diff = np.sqrt(effV - sin_inc_sq) - np.sqrt(effH - sin_inc_sq)
+                print('Effective permittivities=', str(effH), str(effV))
+                print('Del Xeta=', str(xeta_diff))
                 fresh_sd[j, i] = np.abs(np.float32(cpd * WAVELENGTH / (4 * np.pi * xeta_diff)))
             else:
                 fresh_sd[j, i] = np.float32(NO_DATA_VALUE)
 
     print('WRITING UNFILTERED IMAGE...')
-
-    write_tif(fresh_sd, cpd_file_tdx, outfile)
+    write_tif(fresh_sd, avg_cpd_file, outfile)
 
 
 # def get_image_stats(image_arr):
@@ -177,7 +184,8 @@ def cpd2freshsnow(cpd_file_tdx, cpd_file_tsx, layover_file, outfile, neighborhoo
 #     write_tif(img_arr, image_file, outfile)
 
 
-cpd2freshsnow('Out/cpd_tdx_TC.tif', 'Out/cpd_tsx_TC.tif', 'Out/layover.tif', 'fsd')
+do_averaging('Out/cpd_tdx_TC.tif', 'Out/cpd_tsx_TC.tif', 'Fresh_Snow/cpd_avg', 'Fresh_Snow/lia_avg')
+cpd2freshsnow('Fresh_Snow/cpd_avg.tif', 'Fresh_Snow/lia_avg.tif', 'Out/layover.tif', 'Fresh_Snow/fsd')
 #print('UNFILTERED IMAGE VALIDATION...')
 #validate_fresh_snow('Fresh_Snow/Out/fsd.tif', (700097.9845, 3581763.7627), 'val.csv')
 #filter_image('Fresh_Snow/Out/fsd.tif', 'Fresh_Snow/Out/fsd_flt')
